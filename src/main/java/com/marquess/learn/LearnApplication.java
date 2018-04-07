921ac46c77057e280b242c5bd7f33b3d20db6510
dovestyle520 <307908410@qq.com>
2018-04-07 23:48:26 +0800

    下载

--- src/main/java/com/marquess/learn/LearnApplication.java
+++ src/main/java/com/marquess/learn/LearnApplication.java
@@ -13,8 +13,10 @@ import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 //import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan("com.marquess.learn.dao")
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class LearnApplication {
